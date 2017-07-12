7637fa311fdf2f98fa02e9b21c858b728963e21d
Sina Sadeghi <sina.sa@gmail.com>
2017-07-12 22:06:27 +1000

    cleanup and enhance logging

--- vendor/blogir/controller/logger.go
+++ vendor/blogir/controller/logger.go
@@ -3,10 +3,9 @@ package controller
 import (
     "net/http"
     "log"
-    "fmt"
 )
 
 func logger(r * http.Request) {
-    log.Print(fmt.Sprintf("%s %s - %s", r.Proto, r.Method, r.URL))
+    log.Printf("%s %s %s - %s", r.RemoteAddr, r.Proto, r.Method, r.URL)
 }
 
